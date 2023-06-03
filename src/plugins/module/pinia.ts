aa391ac8c95ee3c934f857a93850f10046842775
Hung Anh <anhlhpl52119@gmail.com>
2023-06-03 20:44:44 +0700

    chore: update import module

--- src/plugins/module/pinia.ts
+++ src/plugins/module/pinia.ts
@@ -4,9 +4,6 @@ import type { App } from 'vue';
 const piniaStore = createPinia();
 
 export function setupPiniaStore(app: App<Element>) {
-  // pinia.use(({ store }) => {
-  //   store.router = markRaw(router);
-  // });
   app.use(piniaStore);
 }
 
