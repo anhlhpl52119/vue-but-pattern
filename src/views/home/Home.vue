4222ccb3bf377fc5a0a01deee32f843b47f79450
Hung Anh <anhlhpl52119@gmail.com>
2023-06-03 20:43:59 +0700

    chore: add empty page

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,9 +1,44 @@
 <template>
-  <div>Hello Vue 3</div>
-  <span> this is span text</span>
-  <h1>this is h1</h1>
-  <h2>this is h2</h2>
-  <h3>this is h3</h3>
+  <div class="cont" ref="containerRefs">
+    <section class="hidden">
+      <h1>Hello</h1>
+      <p>this is My website</p>
+    </section>
+    <section class="hidden">
+      <h1>Hello</h1>
+      <p>this is My website</p>
+    </section>
+    <section class="hidden">
+      <h1>Hello</h1>
+      <p>this is My website</p>
+    </section>
+    <section class="hidden">
+      <h1>Hello</h1>
+      <p>this is My website</p>
+    </section>
+    <section class="hidden">
+      <h1>Hello</h1>
+      <p>this is My website</p>
+    </section>
+  </div>
 </template>
-<script lang="ts" setup></script>
-<style scoped></style>
+<script lang="ts" setup>
+onMounted(async () => {
+});
+const containerRefs = ref<HTMLElement>();
+</script>
+<style scoped lang="less">
+section {
+  display: grid;
+  place-items: center;
+  align-items: center;
+  min-height: 100vh;
+}
+.cont {
+  background: #131316;
+  color: #fff;
+  // display: grid;
+  // place-items: center;
+  // min-height: 100vh;
+}
+</style>
---
+++ src/views/main/view1/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>Hello Vue 3 child 1</div>
+</template>
+<script lang="ts" setup>
+
+
+/** Store */
+//const store = useStore();
+
+
+/** Props & Emit */
+//const props = defineProps<{ props: any }>();
+//const {props} = toRefs(props);
+//const emits =defineEmits(['emit']);
+
+
+/** Data */
+
+
+/** Method */
+//function _name() {}
+
+</script>
+<style scoped></style>
