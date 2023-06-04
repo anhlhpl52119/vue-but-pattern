18989f272596edf39d6cd40e820fd4f6add555be
Hung Anh <anhlhpl52119@gmail.com>
2023-06-04 21:26:42 +0700

    feat: add scroll to element util function

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,8 @@
 export const randomPick = (arr: any[]) => {
   return arr[Math.floor(Math.random() * arr.length)];
 };
+export const scrollTo = (refsElement: HTMLElement, timeout?: number) => {
+  setTimeout(() => {
+    refsElement.scrollIntoView({ behavior: 'smooth' });
+  }, timeout);
+};
