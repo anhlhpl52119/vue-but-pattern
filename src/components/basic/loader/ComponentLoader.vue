30f044cdc4c6a9c1bc0725207b7c1a6e7d29bb03
Hung Anh <anhlhpl52119@gmail.com>
2023-06-04 21:28:22 +0700

    feat: add simple loading component

---
+++ src/components/basic/loader/ComponentLoader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div><h1 color="white">Loading....</h1></div>
+</template>
+<script lang="ts" setup>
+/** Store */
+//const store = useStore();
+
+/** Props & Emit */
+//const props = defineProps<{ props: any }>();
+//const {props} = toRefs(props);
+//const emits =defineEmits(['emit']);
+
+/** Data */
+
+/** Method */
+//function _name() {}
+</script>
+<style scoped></style>
