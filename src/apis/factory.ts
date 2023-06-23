76f3329b11bc231573cf93de13e34b4b272d4fdb
Hung Anh <anhlhpl52119@gmail.com>
2023-06-23 22:03:26 +0700

    feat: create repo factory DP

---
+++ src/apis/factory.ts
@@ -0,0 +1,9 @@
+import SomethingApi from './something'
+
+const repository = {
+    something: SomethingApi,
+}
+
+export const useFactor = {
+    basicUse: (name: keyof typeof repository) => repository[name]
+}
