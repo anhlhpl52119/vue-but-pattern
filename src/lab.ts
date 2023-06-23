c0d815f83714043bb05b7c28626dc3cbe3f0b3ea
Hung Anh <anhlhpl52119@gmail.com>
2023-06-23 22:03:05 +0700

    feat: init lab file

--- src/lab.ts
+++ src/lab.ts
@@ -1,26 +1,15 @@
-function getDefaultPrice(price: number) {
-  return price;
-}
-function getNewUserPrice(price: number) {
-  return price - price * 0.2;
-}
-function getBlackFridayPrice(price: number) {
-  return price - price * 0.5;
-}
-function getPromitionPrice(price: number) {
-  return price - price * 0.1 + 50;
-}
+// const hello = new Promise(() => {
+//   return Promise.resolve(
+//     setTimeout(() => {
+//       console.log('hello 300s');
+//     }, 3000)
+//   );
+// });
 
-const typePriceMap = {
-  default: (price: number) => getDefaultPrice(price),
-  newUser: (price: number) => getNewUserPrice(price),
-  blackFriday: (price: number) => getBlackFridayPrice(price),
-  promotion: (price: number) => getPromitionPrice(price),
-};
-type TypePriceTest = keyof typeof typePriceMap;
-export const getPrice = (price: number, type: TypePriceTest) => {
-  return typePriceMap[type](price);
-};
-
-const price = getPrice(200, 'blackFriday');
-console.log(price);
+const pm = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve(console.log('done timeout'));
+  }, 3000);
+});
+const a = await pm
+console.log('pm is: ', a);
