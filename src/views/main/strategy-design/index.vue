f8a446ee83cbf974e9750c0e07c0e72e9891878e
Hung Anh <anhlhpl52119@gmail.com>
2023-06-23 22:06:03 +0700

    feat: init strategy

---
+++ src/views/main/strategy-design/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Hello Vue 3</div>
+</template>
+<script lang="ts" setup>
+/** Store */
+//const store = useStore();
+
+/** Props & Emit */
+//const props = defineProps<{ props: any }>();
+//const {props} = toRefs(props);
+//const emits =defineEmits(['emit']);
+
+/** Data */
+
+/** Method */
+//function _name() {}
+</script>
+<style scoped></style>
