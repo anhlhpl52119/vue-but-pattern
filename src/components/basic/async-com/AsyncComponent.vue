485dfa742e4859076790bde44f9c9a3e0f6866d7
Hung Anh <anhlhpl52119@gmail.com>
2023-06-23 22:04:39 +0700

    feat: add async component

---
+++ src/components/basic/async-com/AsyncComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="testvue">Hello Vue 3 with: {{ textItem }}</div>
+</template>
+<script lang="ts" setup>
+const textItem = ref<string>('');
+
+const loader = async () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      textItem.value = 'asdfasdfasdf';
+      console.log('ok');
+      resolve({
+        sdsd: 'saadf',
+      });
+    }, 2000);
+  });
+};
+
+await loader();
+</script>
+<style scoped>
+.testvue {
+  height: 10rem;
+  width: 30rem;
+  border-radius: 15px;
+  background-color: #fff;
+  color: red;
+}
+</style>
