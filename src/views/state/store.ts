f1597eafadce3bed787a5f1a045932a63dafc885
Hung Anh <anhlhpl52119@gmail.com>
2023-06-29 23:04:41 +0700

    chore: update state

--- src/views/state/store.ts
+++ src/views/state/store.ts
@@ -16,3 +16,54 @@ export function useRouteParams() {
     refRouteQuery,
   };
 }
+function removeItemByName(items, itemName) {
+  return items.filter(item => item.name !== itemName)
+    .map(item => {
+      item.children = removeItemByName(item.children, itemName)
+      return item
+    })
+ }
+ 
+ const input = [
+   {
+     "id": 1,
+     "name": "Viedma",
+     "slug": "viedma",
+     "children": []
+   },
+   {
+     "id": 6,
+     "name": "Cultura",
+     "slug": "Cultura",
+     "children": [
+       {
+         "id": 2,
+         "name": "Rio Negro",
+         "slug": "rio-negro",
+         "children": [
+           {
+             "id": 4,
+             "name": "Edictos",
+             "slug": "edictos",
+             "children": []
+           },
+           {
+             "id": 5,
+             "name": "Deportes",
+             "slug": "deportes",
+             "children": []
+           }
+         ]
+       }
+     ]
+   },
+   {
+     "id": 3,
+     "name": "Policiales",
+     "slug": "policiales",
+     "children": []
+   }
+ ]
+ 
+ console.log(removeItemByName(input, 'Policiales'))
+ console.log(removeItemByName(input, 'Deportes'))
