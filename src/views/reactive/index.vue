ba5cbd258d67fcdbcc02b9418e0a8892620432a3
Hung Anh <anhlhpl52119@gmail.com>
2023-06-30 23:14:26 +0700

    feat: init

---
+++ src/views/reactive/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Hello Vue 3</div>
+    <h3>Component A</h3>
+    <ComponentOne></ComponentOne>
+    <h3>Component B</h3>
+    <ComponentTwo></ComponentTwo>
+  </template>
+  <script lang="ts" setup>
+  
+  </script>
+  <style scoped lang="less">
+  </style>
+  
