7a22179eacbdb7d9147b67d5212dea4e8168dcc1
Hung Anh <anhlhpl52119@gmail.com>
2023-07-01 21:59:36 +0700

    chore: update

--- src/views/reactive/index.vue
+++ src/views/reactive/index.vue
@@ -1,22 +1,33 @@
 <template>
     <div>Hello Vue 3</div>
     <h3>Component A</h3>
-    <button @click="state1.addone">click add {{ state1.count }}</button>
-  <h3>Component Other <pre>{{ state1.item55  }}</pre></h3>
+    <button @click="state1.count++">click add {{ state1.count }}</button>
+    <h3>this Change: {{ state1.changeVl() }}</h3>
     <button @click="state1.setNewItem('sdsd')">Another</button>
+    <button @click="state1.changeoutside">change out</button>
   </template>
   <script lang="ts" setup>
-import {  reactive } from 'vue';
-
+import {  reactive, ref } from 'vue';
+  const controller = ref(150)
   
   const state1 = reactive({
-    count: 1,
+    count: 100,
     item55: {} as {name: string, con: string},
-    addone(){
-        this.count++;
-    },
+    // addone(){
+    //     this.count++;
+    // },
     setNewItem(item: string){
         this.item55.name = item;
+    },
+    changeoutside(){
+        controller.value = 20;
+        console.log(this.count);
+    },
+    changeVl(){
+      const hello = this.count * 2
+      console.log('count:',this.count);
+      console.log('object', hello);
+      return `hello this is curent count: ${hello}`
     }
   });
 
