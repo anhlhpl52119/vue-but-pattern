0e86a4237ff57d46f6a1c57b8becbffed5ce61eb
Hung Anh <anhlhpl52119@gmail.com>
2023-07-01 22:42:50 +0700

    chore: update

--- src/views/reactive/index.vue
+++ src/views/reactive/index.vue
@@ -2,20 +2,22 @@
     <div>Hello Vue 3</div>
     <h3>Component A</h3>
     <button @click="state1.count++">click add {{ state1.count }}</button>
-    <h3>this Change: {{ state1.changeVl() }}</h3>
+    <h3><pre>{{ state1.converted }}</pre></h3>
     <button @click="state1.setNewItem('sdsd')">Another</button>
     <button @click="state1.changeoutside">change out</button>
   </template>
   <script lang="ts" setup>
 import {  reactive, ref } from 'vue';
   const controller = ref(150)
-  
+  interface Converted {
+    name: string,
+    age: number,
+  }
   const state1 = reactive({
     count: 100,
     item55: {} as {name: string, con: string},
-    // addone(){
-    //     this.count++;
-    // },
+    converted: {} as Converted,
+
     setNewItem(item: string){
         this.item55.name = item;
     },
@@ -28,7 +30,15 @@ import {  reactive, ref } from 'vue';
       console.log('count:',this.count);
       console.log('object', hello);
       return `hello this is curent count: ${hello}`
-    }
+    },
+    _setconver() {
+      return this.count++;
+      // console.log('run');
+      // this.converted = {
+      //   name: `thisi Name: ${this.count}`,
+      //   age: this.count-1 
+      // };
+      },
   });
 
   </script>
